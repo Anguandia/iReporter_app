8594559ee434e17dd1a145acdd3fe34128c03e17
anguandia mike <mikeanguandia@gmail.com>
2018-12-27 02:39:41 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,7 @@ from config import app_config
 
 
 def create_app(config_name):
-    app = FlaskAPI(__name__, instance_relative_config=True)
-    app.config.from_pyfile('config.py')
+    app = FlaskAPI(__name__)
     app.config.from_object(app_config[config_name])
 
     return app
