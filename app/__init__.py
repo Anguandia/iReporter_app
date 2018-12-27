3db0aeb06d8f9d25cf5bb4568967ed5bd3b74ea2
Anguandia <mikeanguandia@gmail.com>
2018-12-27 08:39:32 +0300

    learn config

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,8 @@ from config import app_config
 
 
 def create_app(config_name):
-    app = FlaskAPI(__name__, instance_relative_config=True)
+    app = FlaskAPI(__name__)
     app.config.from_object('config')
-    app.config.from_pyfile('config.py')
     app.config.from_object(app_config[config_name])
 
     return app
